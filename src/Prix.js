824ab6ae70c125a25dafc964694fc07ee43290d4
Aba221 <thiawa27@live.fr>
2023-09-06 10:07:02 +0000

    first commit

---
+++ src/Prix.js
@@ -0,0 +1,8 @@
+import React from "react";
+import product from "./product";
+
+function Price() {
+  return <p>Prix : {product.price} FCFA</p>;
+}
+
+export default Price;
---
+++ src/product.js
@@ -0,0 +1,9 @@
+const product = {
+  name: "Carotte",
+  price: 1200,
+  description: "Les carottes sont une source importante de vitamines, en particulier la vitamine A, ainsi que de fibres alimentaires. Elles sont souvent consommées crues en salade, comme collation, ou utilisées dans une variété de plats cuits, tels que les soupes, les ragoûts, les sautés et les purées. ",
+  image: "/image.jpg",
+};
+
+
+export default product;
